15bdd609ae0524940c1889b589dece2a410cf51b
Zachary Miller <millerz@ufl.edu>
2025-02-17 23:18:03 -0500

    Audio Fixes
    Made changes to most recent branch, fixed issue with volume between scenes, added Audio Manager to persist changes

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance; // Singleton to ensure one instance exists
+    public AudioMixer mixer;
+
+    private void Awake()
+    {
+        // Singleton to share across scenes
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        LoadVolume();
+    }
+
+    public void LoadVolume()
+    {
+        float value = PlayerPrefs.GetFloat("Volume", 0.5f);
+        ApplyVolume(value);
+    }
+
+    public void ApplyVolume(float value)
+    {
+        if (value <= 0)
+        {
+            mixer.SetFloat("Volume", -144);
+        }
+        else
+        {
+            mixer.SetFloat("Volume", Mathf.Log10(value) * 20);
+        }
+    }
+
+    public void SaveVolume(float value)
+    {
+        PlayerPrefs.SetFloat("Volume", value);
+        PlayerPrefs.Save();
+        ApplyVolume(value);
+    }
+}
---
+++ Assets/Scripts/ShowMenu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class ShowMenu : MonoBehaviour
+{
+    public GameObject element;
+
+    public void ShowElement()
+    {
+        element.SetActive(!element.activeSelf);
+    }
+}
