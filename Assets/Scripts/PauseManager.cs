e7814df9211dbaf286ffc240441f77f3d16a64d5
Zachary Miller <millerz@ufl.edu>
2025-02-21 20:58:47 -0500

    Added Pause Menu
    Added pause menu and Money UI to movementTest scene

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseGame();
+        }
+    }
+
+    public void pauseGame()
+    {
+        if (pauseMenu.activeSelf)
+        {
+            //Time.timeScale = 1.0f;
+            pauseMenu.SetActive(false);
+        }
+        else
+        {
+            //Time.timeScale = 0.0f;
+            pauseMenu.SetActive(true);
+        }
+    }
+}
