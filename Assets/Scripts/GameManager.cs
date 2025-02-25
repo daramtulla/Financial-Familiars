adf5f0ef8d043011e0b5ce7c11fe9f221fea1c9c
Zachary Miller <millerz@ufl.edu>
2025-02-25 01:12:33 -0500

    Added End of Day features
    Added End of Day button, end of day screen, and money logic to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +10,15 @@ public class GameManager : MonoBehaviour
     public SuppliersMenu suppliersMenu;
     public PlayerManager playerManager;
 
+    //End of Day Visuals
+    public GameObject endScreen;
+    public Text endDayTitle;
+    public Text moneyMade;
+    public Text loansPaid;
+    public Text wagesPaid;
+    public Text upgradeUpkeep;
+    public Text netProfit;
+
     public void Start()
     {
     }
@@ -16,13 +26,63 @@ public class GameManager : MonoBehaviour
     public void endDay()
     {
         //Sell Items
-        float moneyMade = inventoryMenu.sellItems();
+        float moneyMadeAmount = inventoryMenu.sellItems();
+
+        //Display End of Day
+        endScreen.SetActive(true);
+        endDayTitle.text = "Day " + playerManager.getDay().ToString() + " Results";
+        //Money Made
+        if(moneyMadeAmount > 0)
+        {
+            moneyMade.text = "$" + moneyMadeAmount.ToString();
+            moneyMade.color = Color.green;
+        }
+        else if (moneyMadeAmount < 0)
+        {
+            moneyMade.text = "-$" + (-moneyMadeAmount).ToString();
+            moneyMade.color = Color.red;
+        }
+        else
+        {
+            moneyMade.text = "$" + moneyMadeAmount.ToString();
+            moneyMade.color = Color.gray;
+        }
+        //TODO: Add loan logic
+        //For now: say it takes $100 to pay off every day
+        loansPaid.color = Color.red;
+        loansPaid.text = "-$100";
+
+        //TODO: Add hiring mechanic
+        //For now: set Wages paid to 0 for 0 employees
+        wagesPaid.color = Color.gray;
+        wagesPaid.text = "$0";
+
+        //TODO: Add Upgrade Mechanics
+        //For now: set Upgrades to 0 for no upgrades
+        upgradeUpkeep.color = Color.gray;
+        upgradeUpkeep.text = "$0";
+
+        //TODO: Add Loan, Hiring, and Upgrades to current calculation
+        float netProfitAmount = moneyMadeAmount - 100;
+        if (netProfitAmount > 0)
+        {
+            netProfit.text = "$" + netProfitAmount.ToString();
+            netProfit.color = Color.green;
+        }
+        else if (netProfitAmount < 0)
+        {
+            netProfit.text = "-$" + (-netProfitAmount).ToString();
+            netProfit.color = Color.red;
+        }
+        else
+        {
+            netProfit.text = "$" + netProfitAmount.ToString();
+            netProfit.color = Color.gray;
+        }
 
         //TODO: Split money between savings and spending
-
         //For now: Send all money to one account
-        playerManager.endDayStats(moneyMade);
-
+        playerManager.updatePlayerStats(netProfitAmount);
     }
 
     public void nextDay()
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -11,12 +11,26 @@ public class PlayerManager : MonoBehaviour
     void Start()
     {
         money = PlayerPrefs.GetFloat("amount");
-        moneyCount.text = money.ToString("0.##");
+        moneyCount.text = "$" + money.ToString("0.##");
+        if (money > 0)
+        {
+            moneyCount.color = new Color(1.0f, 0.5f, 0);
+        }
+        else if (money < 0)
+        {
+            moneyCount.text = "-$" + (-money).ToString();
+            moneyCount.color = Color.red;
+        }
+        else
+        {
+            moneyCount.color = new Color(1.0f, 0.5f, 0);
+        }
+
         day = PlayerPrefs.GetInt("day");
         dayCount.text = "Day " + day.ToString();
 
     }
-    public void endDayStats(float moneyMade)
+    public void updatePlayerStats(float moneyMade)
     {
         //Increment day
         day += 1;
@@ -27,17 +41,37 @@ public class PlayerManager : MonoBehaviour
         money += moneyMade;
         PlayerPrefs.SetFloat("amount", money);
         PlayerPrefs.Save();
-        moneyCount.text = money.ToString();
+
+        moneyCount.text = "$" + money.ToString("0.##");
+        if (money > 0)
+        {
+            moneyCount.color = new Color(1.0f, 0.5f, 0);
+        }
+        else if (money < 0)
+        {
+            moneyCount.text = "-$" + (-money).ToString();
+            moneyCount.color = Color.red;
+        }
+        else
+        {
+            moneyCount.color = new Color(1.0f, 0.5f, 0);
+        }
     }
 
     public void resetData()
     {
-        moneyCount.text = "0000";
+        moneyCount.text = "$0000";
+        moneyCount.color = new Color(1.0f, 0.5f, 0);
         dayCount.text = "Day 1";
         money = 0;
         day = 1;
         PlayerPrefs.SetInt("day", 1);
         PlayerPrefs.SetFloat("amount", 0);
         PlayerPrefs.Save();
+    }
+
+    public int getDay()
+    {
+        return day;
     }
 }
