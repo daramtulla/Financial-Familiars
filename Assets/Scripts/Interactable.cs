3ad8a5618f5818cdba3bdef19e3237abd77753de
GongBingWong <123975332+GongBingWong@users.noreply.github.com>
2025-02-26 20:29:10 -0500

    Connect cauldron to suppliers menu
    Still buggy at the moment (failing to open menu sometimes and also player walking animation playing and player rotation when "frozen").

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -2,11 +2,25 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour, Interact
 {
+    [SerializeField] GameObject menuToOpenOrClose;
     public void Interact()
     {
         Debug.Log("Interacting");
 
         //Insert functionality here
+
+        //If the menu is open
+        if(menuToOpenOrClose.activeInHierarchy)
+        {
+            //Close the menu
+            menuToOpenOrClose.SetActive(false);
+        }
+        //Else the menu is closed
+        else
+        {
+            //Open the menu
+            menuToOpenOrClose.SetActive(true);
+        }
 
     }
 
