a9903a651d227bc558c1602b07eb7d033907177a
zach112 <122427374+zachj112@users.noreply.github.com>
2025-02-27 19:14:55 -0500

    Added Merch items and stats to JSON database

--- Assets/Scripts/JSONDatabaseOperations.cs
+++ Assets/Scripts/JSONDatabaseOperations.cs
@@ -16,8 +16,19 @@ public class JSONDatabaseOperations : MonoBehaviour
         currentPlayer = new Player();
         filePath = Application.persistentDataPath + "/JSONDatabase.json";
 
-        //TEST REMOVE WHEN DONE
-        //currentPlayer.merch.Add(new Merchandise(1, "hi", 1, 1.1, 1.1, 1.1, 1.1));
+        //Preintailize items in list to 0 inventory and markup. Change on load.
+        currentPlayer.merch.Add(new Merchandise(1, "Crimson Restoration Potion", 0, 10, 0, .2f, 1, 1));
+        currentPlayer.merch.Add(new Merchandise(2, "Bubbling Polymorph Flask", 0, 25, 0, .2f, 1, 2));
+        currentPlayer.merch.Add(new Merchandise(3, "Draught of Living Death", 0, 65, 0, .2f, 1, 3));
+        currentPlayer.merch.Add(new Merchandise(4, "Emerald Ring of Minor Protection", 0, 30, 0, .15f, 2, 1));
+        currentPlayer.merch.Add(new Merchandise(5, "Necklace of Fire Resistance", 0, 70, 0, .15f, 2, 2));
+        currentPlayer.merch.Add(new Merchandise(6, "Pocket Dimension Bag", 0, 220, 0, .15f, 2, 3));
+        currentPlayer.merch.Add(new Merchandise(7, "Never-Dull Gold Dagger", 0, 150, 0, .10f, 3, 1));
+        currentPlayer.merch.Add(new Merchandise(8, "Lich King Grimoire", 0, 320, 0, .10f, 3, 2));
+        currentPlayer.merch.Add(new Merchandise(9, "Featherlight Warhammer", 0, 700, 0, .10f, 3, 3));
+        currentPlayer.merch.Add(new Merchandise(10, "All-Seeing Crystal Ball", 0, 200, 0, .10f, 4, 1));
+        currentPlayer.merch.Add(new Merchandise(11, "Wand Core Cluster", 0, 200, 0, .10f, 4, 2));
+        currentPlayer.merch.Add(new Merchandise(12, "Pulsating Dragon Heart", 0, 200, 0, .10f, 4, 3));
     }
 
     private void SaveData()
@@ -45,6 +56,7 @@ public class JSONDatabaseOperations : MonoBehaviour
 
     void Update()
     {
+        //For debugging
         //When quiting from pause menu, call this function TODO
         if (debug && Input.GetKey(KeyCode.K))
         {
@@ -66,11 +78,51 @@ public class Player
     public int currentMoney;
 
     public List<Merchandise> merch = new List<Merchandise>();
-
-    //TODO: Do we want to save this to the player or save game?
     public int currentLoanAmount;
+    public void changeQuantity(int id, int change)
+    {
+        if (id < 1 || id > 13)
+        {
+            Debug.Log("Invalid ItemId");
+            return;
+        }
+        else
+        {
+            if (merch[id].quantity + change > -1)
+            {
+                merch[id].quantity += change;
+            }
+            else
+            {
+                Debug.Log("Not enough stock");
+                return;
+            }
+        }
 
-    //Implement skills TODO
+        return;
+    }
+
+    public void changeMarkup(int id, float newMarkup)
+    {
+        if (id < 1 || id > 13)
+        {
+            Debug.Log("Invalid ItemId");
+            return;
+        }
+        else
+        {
+            //limiting markup limits
+            if ((newMarkup > .5f || newMarkup < -.5f))
+            {
+                Debug.Log("Invalid ItemId");
+                return;
+            }
+            else
+            {
+                merch[id].markupPercentage = newMarkup;
+            }
+        }
+    }
 }
 
 [System.Serializable]
@@ -80,16 +132,22 @@ public class Merchandise
     public int id;
     public string name;
     public int quantity;
-    public float cost;
+    public float baseCost;
     public float markupPercentage;
-    public float slope;
-    public float intercept;
+    public float customerMod;
+    public int group;
+    public int tier;
 
-    /*
-        public Merchandise(int id, string name, int quantity, float cost, float markupPercentage, float slope, float intercept)
-        {
-            this.id = id;
 
-        }
-    */
+    public Merchandise(int id, string name, int quantity, float baseCost, float markupPercentage, float customerMod, int group, int tier)
+    {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.baseCost = baseCost;
+        this.markupPercentage = markupPercentage;
+        this.customerMod = customerMod;
+        this.group = group;
+        this.tier = tier;
+    }
 }
