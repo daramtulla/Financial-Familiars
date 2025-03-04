bf2b2ebbf6cbbb987277f76488a9424990635e31
zach112 <122427374+zachj112@users.noreply.github.com>
2025-03-04 02:21:28 -0500

    Revert "Added Loan Manager Package (from David's Branch)"
    This reverts commit 06ce7bc26865f5411c93e93870dd917963182d4c.

--- Assets/Scripts/InventoryMenu.cs
+++ Assets/Scripts/InventoryMenu.cs
@@ -1,35 +1,43 @@
 using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
+using System.IO;
+using System.Data.Common;
 public class InventoryMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject inventoryPanel;
+    [SerializeField] InteractionManager interactionManager;
 
     //cmon scrollbar!
     public Transform inventoryContent;
     public GameObject inventoryItemPrefab;
 
-    //all the stock
-    private List<InventoryItem> inventory = new List<InventoryItem>();
-        
+    //For selling items
+    private CustomerManager customerManager;
+
+    public JSONDatabaseOperations db;
+
     void Start()
     {
         inventoryPanel.SetActive(false);
-        AddTestItems();
+        //AddTestItems();
         UpdateInventoryUI();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //press I to open inventory
-        if(Input.GetKeyDown(KeyCode.I)){
+        if (Input.GetKeyDown(KeyCode.I))
+        {
             ToggleMenu();
         }
     }
 
-    public void ToggleMenu(){
+    public void ToggleMenu()
+    {
         inventoryPanel.SetActive(!inventoryPanel.activeSelf);
         if (inventoryPanel.activeSelf)
         {
@@ -37,19 +45,22 @@ public class InventoryMenu : MonoBehaviour
         }
     }
 
-    public void CloseMenu(){
-        inventoryPanel.SetActive(false);
-
-    }
-
-    private void AddTestItems()
+    public void CloseMenu()
     {
-        inventory.Add(new InventoryItem(1, "Clear Crystal Orb", 10, 20.00f, 50));
-        inventory.Add(new InventoryItem(2, "Walnut Wand", 100, 10.00f, 25));
-        inventory.Add(new InventoryItem(3, "Vial of Acid", 1, 25.00f, 100));
+        Debug.Log("Test Inventory Menu Close");
+        //The if statement prevents you from being frozen when you press I to open the inventory menu
+        //and then press the close button.
+        //Also prevents "switchInteractState()" from being called twice when F is pressed
+        //(once in the Update() function of InteractionManager.cs and the other time here).
+        if (InteractionManager.GetInteractState() == true)
+        {
+            Debug.Log("(InventoryMenu): GetInteractState() is true");
+            interactionManager.switchInteractState();
+        }
+        inventoryPanel.SetActive(false);
     }
 
-    private void UpdateInventoryUI()
+    public void UpdateInventoryUI()
     {
         // Clear existing UI items
         foreach (Transform child in inventoryContent)
@@ -57,22 +68,50 @@ public class InventoryMenu : MonoBehaviour
             Destroy(child.gameObject);
         }
 
+        List<Merchandise> inventory = db.currentPlayer.merch;
+
         // Populate the inventory UI
-        foreach (InventoryItem item in inventory)
+        foreach (Merchandise item in inventory)
         {
             GameObject newItem = Instantiate(inventoryItemPrefab, inventoryContent);
-            
+
+            float sellingPrice = item.baseCost * item.markupPercentage;
+
             // Find TextMeshPro components directly in the prefab
             TextMeshProUGUI[] texts = newItem.GetComponentsInChildren<TextMeshProUGUI>();
-            texts[0].text = item.idNumber.ToString();
-            texts[1].text = item.itemName;
-            texts[2].text = item.quantity.ToString();
-            texts[3].text = "$" + item.baseCost.ToString("0.00");
-            texts[4].text = item.markup.ToString("0") + "%";
-            texts[5].text = "$" + item.SellingPrice.ToString("0.00");
-            texts[6].text = "$" + item.Profit.ToString("0.00");
+            texts[0].text = item.name;
+            texts[1].text = item.quantity.ToString();
+            texts[2].text = "$" + item.baseCost.ToString();
+            texts[3].text = item.markupPercentage.ToString() + "%";
+            texts[4].text = item.group.ToString();
+            texts[5].text = item.tier.ToString();
         }
     }
 
+    public void ReloadInventory()
+    {
+        Debug.Log("Reloading Inventory...");
 
+        UpdateInventoryUI();
+        Canvas.ForceUpdateCanvases();
+        inventoryPanel.SetActive(false);
+        inventoryPanel.SetActive(true);
+        Canvas.ForceUpdateCanvases();
+
+        Debug.Log("Inventory Reloaded and UI Updated.");
+    }
+
+    public void SellItems()
+    {
+        foreach (Merchandise merch in db.currentPlayer.merch)
+        {
+            float degrees = merch.markupPercentage / merch.customerMod;
+
+            //100 is a place holder value for now
+            float sales = merch.baseCost * merch.markupPercentage * (100 - (100 * merch.customerMod * degrees));
+
+            db.currentPlayer.currentMoney += sales;
+            db.UpdateDailySales(sales);
+        }
+    }
 }
