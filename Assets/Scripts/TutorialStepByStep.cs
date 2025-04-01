77130da7ac16d6d3406674de9483ce187fd541d2
AthenaWrenn <162925464+AthenaWrenn@users.noreply.github.com>
2025-04-01 13:17:53 -0400

    Tutorial Ordering
    Step 1 into Step 2

--- Assets/Scripts/TutorialStepByStep.cs
+++ Assets/Scripts/TutorialStepByStep.cs
@@ -7,10 +7,32 @@ public class TutorialPlaybyPlay : MonoBehaviour
     public GameObject SpotCauldron;
     public GameObject SpotLectern;
 
+    private int chestUses = 0;
+    private bool cauldronUsed = false;
+    private bool lecternUsed = false;
+
 
     void Start()
     {
         StartCoroutine(PlayTutorial());
+    }
+
+    public void NotifyTutorialObjectUsed(string id)
+    {
+        Debug.Log("Tutorial object used: " + id);
+
+        switch (id)
+        {
+            case "InventoryChest":
+                chestUses++;
+                break;
+            case "cauldron":
+                cauldronUsed = true;
+                break;
+            case "lectern":
+                lecternUsed = true;
+                break;
+        }
     }
 
     IEnumerator PlayTutorial()
@@ -21,6 +43,11 @@ public class TutorialPlaybyPlay : MonoBehaviour
         SpotChest.SetActive(true);
         SpotCauldron.SetActive(false);
         SpotLectern.SetActive(false);
+        yield return new WaitUntil(() => chestUses >= 2);
+
+        //Step 2: Go to cauldron
+        SpotChest.SetActive(false);
+        SpotCauldron.SetActive(true);
 
 
         Debug.Log("Tutorial complete!");
