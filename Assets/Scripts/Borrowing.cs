8706a7a36699fd5093ed55964f93b5920ef6ae62
zach112 <122427374+zachj112@users.noreply.github.com>
2025-04-21 14:16:19 -0400

    Interest format fix

--- Assets/Scripts/Borrowing.cs
+++ Assets/Scripts/Borrowing.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
 using System.Data.Common;
+using System;
 
 public class Borrowing : MonoBehaviour
 {
@@ -99,7 +100,8 @@ public class Borrowing : MonoBehaviour
         foreach (TextMeshProUGUI t in texts)
         {
             if (t.name == "Amount") t.text = $"${loan.amount}";
-            else if (t.name == "Interest") t.text = $"{loan.interest}%";
+            else if (t.name == "Interest") t.text = $"{loan.interest * 100}%";
+            //
             else if (t.name == "Lender") t.text = $"{loan.lender}";
             else if (t.name == "InterestType") t.text = $"{loan.type.ToString()}";
         }
